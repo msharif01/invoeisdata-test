37183b2f957c09f586828c224a65b829428f4500
msharif01 <msharifismail01@gmail.com>
2023-07-03 17:26:34 +0800

    styles: disable black bacground & add blue span edge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,12 @@ export default function Home() {
         <div className="min-h-screen flex flex-col gap-8 w-full max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl mx-auto py-4 sm:py-8 md:py-16">
           <div className="flex-grow overflow-y-auto">
             {/* First Page */}
-            <div className="min-h-screen bg-gray-100 p-4">
-              <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4">First Page</h1>
-              <p className="text-sm sm:text-base md:text-lg">
+            <div className="min-h-screen p-4 relative">
+              <div className="absolute left-0 top-0 h-full bg-blue-900 w-1/3"></div>
+              <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 relative z-10">
+                First Page
+              </h1>
+              <p className="text-sm sm:text-base md:text-lg relative z-10">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
               </p>
             </div>
@@ -15,9 +18,12 @@ export default function Home() {
 
           <div className="flex-grow overflow-y-auto">
             {/* Second Page */}
-            <div className="min-h-screen bg-gray-200 p-4">
-              <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4">Second Page</h1>
-              <p className="text-sm sm:text-base md:text-lg">
+            <div className="min-h-screen p-4 relative">
+              <div className="absolute left-0 top-0 h-full bg-blue-900 w-1/3"></div>
+              <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4 relative z-10">
+                Second Page
+              </h1>
+              <p className="text-sm sm:text-base md:text-lg relative z-10">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...
               </p>
             </div>
